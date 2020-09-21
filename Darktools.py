a5d7d57ca1c7db6e90da7277ed650153cab2a7d2
Dark Z <70073501+darkzhacker@users.noreply.github.com>
2020-09-21 17:08:32 +0600

    Add files via upload

---
+++ Darktools.py
@@ -0,0 +1,209 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+# Jangan Recode Ya gayn
+
+echo
+echo
+echo
+    echo $i"       [•]"$me"─────────────────────────────────────────── "$i"[•]"
+     echo $i"        |"$cy"                        DARK Z corporation ©      "$i"|"
+     echo $i"        |"$me"───────────────────────────────────────────── "$i"|"
+     echo $i"        |"$pu"    Auther :"$bi" Team Dark Z             "$i"|"
+     echo $i"        |"$pu"    Facebook :"$bi" Dark Z                "$i"|"
+     echo $i"        |"$pu"    Contact WhatsApp :"$bi" +8801978066581            "$i"|"
+     echo $i"        |"$pu"   Contact Gmail :"$bi" darkzgaming22@gmail.com   "$i"|"
+    echo $i"       [•]"$me"────────────────────────────────────────────"$i"[•]"
+echo
+
+
+echo
+echo
+echo
+echo $cy"       ░█▀▀"$i"░░░█▀█░░░"$me"█▀▀░░░"$ku"█▀▀░░░"$cy"█▀▄░░░"$i"█▀█░░░"$bi"█▀█░░░"$pur"█░█"
+echo $bi"       ░█▀▀░░░"$cy"█▀█░░░█░░░░░"$bi"█▀▀░░░"$i"█"$cy"▀▄░░░█░"$me"█░░░█░█░░░"$ku"█▀▄"
+echo $pur"       ░▀░░░░░▀░▀░░░▀"$bi"▀▀░░░▀▀"$i"▀░░░▀▀░░░░"$me"▀▀▀░░░"$cy"▀▀▀░░░▀░"$bi"▀"
+echo
+
+
+
+
+
+
+
+echo
+echo
+echo
+      echo $ku"        ★"$i"##########################################"$ku"★"
+      echo $ku"        ★"$cy"   |"$me"1"$cy"|"$pur"  Facebook Auto Tools"$ku"               ★"
+      echo $ku"        ★"$cy"   |"$me"2"$cy"|"$pur"  Facebook Hack"$i" [Phishing]"$ku"          ★"
+      echo $ku"        ★"$cy"   |"$me"3"$cy"|"$pur"  Facebook Hack"$i" [BruteForce]"$ku"        ★"
+      echo $ku"        ★"$cy"   |"$me"4"$cy"|"$pur"  Facebook Auto Report"$ku"              ★"
+      echo $ku"        ★"$i"##########################################"$ku"★"
+echo
+    
+
+
+echo
+echo
+echo
+echo $i"        [1] Install All Packages" 
+echo $me"        [0] EXIT"
+echo
+    
+
+
+echo $me"╔═══"$bi"["$i"BD"$bi"]"$me"══════"$bi"["$i""Team DarkZ""$bi"]"
+echo $me"║"
+read -p"╚═══➣➣ " pil
+
+
+if [ $pil = 1 ]
+then
+clear
+echo
+echo
+echo
+echo $pur"
+██████╗░░█████╗░██████╗░██╗░░██╗  ███████╗
+██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ╚════██║
+██║░░██║███████║██████╔╝█████═╝░  ░░███╔═╝
+██║░░██║██╔══██║██╔══██╗██╔═██╗░  ██╔══╝░░
+██████╔╝██║░░██║██║░░██║██║░╚██╗  ███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝  ╚══════╝"
+echo
+sleep 1
+git clone https://github.com/ciku370/OSIF
+cd OSIF	
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo
+echo
+echo
+echo $cy"
+██████╗░░█████╗░██████╗░██╗░░██╗  ███████╗
+██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ╚════██║
+██║░░██║███████║██████╔╝█████═╝░  ░░███╔═╝
+██║░░██║██╔══██║██╔══██╗██╔═██╗░  ██╔══╝░░
+██████╔╝██║░░██║██║░░██║██║░╚██╗  ███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝  ╚══════╝"
+echo
+sleep 1
+pkg install python3 -y
+pkg install pip -y
+pkg install pyhon -y
+pkg install git python php curl openssh grep
+git clone -b Termux-Support-Branch https://github.com/DarkSecDevelopers/HiddenEye.git 
+chmod 777 HiddenEye 
+cd HiddenEye 
+pip install -r requirements.txt 
+pip install requests 
+python HiddenEye.py 
+fi
+
+if [ $pil = 3 ]
+then
+clear
+echo
+echo
+echo
+echo $i"
+██████╗░░█████╗░██████╗░██╗░░██╗  ███████╗
+██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ╚════██║
+██║░░██║███████║██████╔╝█████═╝░  ░░███╔═╝
+██║░░██║██╔══██║██╔══██╗██╔═██╗░  ██╔══╝░░
+██████╔╝██║░░██║██║░░██║██║░╚██╗  ███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝  ╚══════╝"
+echo
+sleep 1
+git clone https://github.com/darkzhacker/Facebook-BruteForcer
+cd Facebook-BruteForcer    
+pip3 install requests bs4
+pip install mechanize
+pip install requests
+pip2 install requests
+pip2 install mechanize
+ls
+chmod +x faceboom.py
+ls
+python2 faceboom.py
+
+if [ $pil = 5 ]
+then
+clear
+apt update && apt upgrade
+apt install python2 -y
+pkg install python3
+pkg install python2 -y
+pkg install python
+pkg install git -y
+pip2 install urllib3 chardet certifi idna requests
+pkg install openssh -y
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+
+pkg install php -y
+pkg install ruby cowsay toilet figlet -y
+pkg install neofetch
+pkg install nano
+toilet -f pagga "S U C C E S S" | lolcat
+fi
+
+
+if [ $pil = 0 ]
+then
+clear
+echo
+echo
+echo
+echo $me"    ______   _  __    ____   ______
+   / ____/  | |/ /   /  _/  /_  __/
+  / __/     |   /    / /     / /
+ / /___    /   |   _/ /     / /
+/_____/   /_/|_|  /___/    /_/"
+echo
+sleep 2
+echo $cy" Thanks for support us "
+sleep 2
+exit
+fi
+
+if [ $pil = 4 ]
+then
+clear
+echo
+echo
+echo
+echo $bi"
+██████╗░░█████╗░██████╗░██╗░░██╗  ███████╗
+██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ╚════██║
+██║░░██║███████║██████╔╝█████═╝░  ░░███╔═╝
+██║░░██║██╔══██║██╔══██╗██╔═██╗░  ██╔══╝░░
+██████╔╝██║░░██║██║░░██║██║░╚██╗  ███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝  ╚══════╝"
+echo
+sleep 1
+git clone https://github.com/IlayTamvan/Report 
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+
+
+
